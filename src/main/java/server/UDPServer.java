86506ec0c1df7f4d176c41b57fe047a5a49242ad
shaemhameforash <Jaroslaw@TheMightySteed>
2015-02-28 19:00:42 +0000

    COMMIT TEST

--- src/main/java/server/UDPServer.java
+++ src/main/java/server/UDPServer.java
@@ -5,6 +5,7 @@ import java.net.*;
 public class UDPServer {
 	 public static void main(String args[]) throws Exception
      {
+		String gittest = "test";
         @SuppressWarnings("resource")
 		DatagramSocket serverSocket = new DatagramSocket(9876);
            byte[] receiveData = new byte[1024];
