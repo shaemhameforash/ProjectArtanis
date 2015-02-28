bac06d458c2ae893577ce6f9a88e92f8c175c973
shaemhameforash <Jaroslaw@TheMightySteed>
2015-02-28 19:07:52 +0000

    WTF?

--- src/main/java/server/UDPServer.java
+++ src/main/java/server/UDPServer.java
@@ -5,7 +5,7 @@ import java.net.*;
 public class UDPServer {
 	 public static void main(String args[]) throws Exception
      {
-        //TESTING THE COMMITS AGAAAAAAAAAAIN
+        String WTF = "?";
 		 @SuppressWarnings("resource")
 		DatagramSocket serverSocket = new DatagramSocket(9876);
            byte[] receiveData = new byte[1024];
