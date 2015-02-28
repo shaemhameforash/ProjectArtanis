146bef93c3f11abdf5ea3e5d0384c1b14967d57c
shaemhameforash <Jaroslaw@TheMightySteed>
2015-02-28 18:50:35 +0000

    INITIAL COMMIT

---
+++ src/main/java/server/UDPServer.java
@@ -0,0 +1,27 @@
+package server;
+
+import java.net.*;
+
+public class UDPServer {
+	 public static void main(String args[]) throws Exception
+     {
+        @SuppressWarnings("resource")
+		DatagramSocket serverSocket = new DatagramSocket(9876);
+           byte[] receiveData = new byte[1024];
+           byte[] sendData = new byte[1024];
+           DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+           while(true)
+              {
+                 serverSocket.receive(receivePacket);
+                 String sentence = new String( receivePacket.getData());
+                 System.out.println("RECEIVED: " + sentence);
+                 InetAddress IPAddress = receivePacket.getAddress();
+                 int port = receivePacket.getPort();
+                 String capitalizedSentence = sentence.toUpperCase();
+                 sendData = capitalizedSentence.getBytes();
+                 DatagramPacket sendPacket =
+                 new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                 serverSocket.send(sendPacket);
+              }
+     }
+}
