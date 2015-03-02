b27b01cf94e479c34b748da636269d54854c1b04
shaemhameforash <ucc111759775@gmail.com>
2015-03-02 21:20:03 +0000

    Added MainMenu and ArtanisGame classes

---
+++ src/main/java/logic/ArtanisGame.java
@@ -0,0 +1,15 @@
+package logic;
+
+import multiplayer.UDPClient;
+import multiplayer.UDPServer;
+
+public class ArtanisGame {
+
+	UDPClient client = new UDPClient();
+	UDPServer server = new UDPServer();
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/main/java/multiplayer/UDPClient.java
+++ src/main/java/multiplayer/UDPClient.java
@@ -1,27 +1,26 @@
 package multiplayer;
 
-import java.io.*;
 import java.net.*;
 
-class UDPClient
-{
-   public static void main(String args[]) throws Exception
-   {
-	  while(true){
-			      DatagramSocket clientSocket = new DatagramSocket();
-			      InetAddress IPAddress = InetAddress.getByName("52.16.70.139");
-			      byte[] sendData = new byte[1024];
-			      byte[] receiveData = new byte[1024];
-			      String sentence;
-			      sentence ="PING";
-			      sendData = sentence.getBytes();
-			      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-			      clientSocket.send(sendPacket);
-			      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			      clientSocket.receive(receivePacket);
-			      String modifiedSentence = new String(receivePacket.getData());
-			      System.out.println("FROM SERVER:" + modifiedSentence);
-			      clientSocket.close();
-	  }
-   }
+public class UDPClient {
+	public static void initialize() throws Exception {
+		while (true) {
+			DatagramSocket clientSocket = new DatagramSocket();
+			InetAddress IPAddress = InetAddress.getByName("52.16.70.139");
+			byte[] sendData = new byte[1024];
+			byte[] receiveData = new byte[1024];
+			String sentence;
+			sentence = "PING";
+			sendData = sentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData,
+					sendData.length, IPAddress, 9876);
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+					receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence);
+			clientSocket.close();
+		}
+	}
 }
